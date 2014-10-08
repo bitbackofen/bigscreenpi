7a35d31a939f5eb806eff9449cd03963b7e3fdfe
Paul Busch <paul@iMac.local>
2014-10-08 12:14:32 +0200

    first test for playing videos in folder

--- videoplayer.sh
+++ videoplayer.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+PLAYER='omxplayer'
+OPTIONS='-rb --no-osd -o both'
+
+PATH='/var/videos'
+PREFIX='movie_'
+LOG='/var/log/video.log'
+PAUSENVIDEO='/var/video/SNR_Pausenstueck.mp4'
+
+setterm -blank off -powerdown off > /dev/tty0
+clear > /dev/tty0
+setterm -cursor off > /dev/tty0
+
 while :
 do
-	setterm -blank off -powerdown off > /dev/tty0
-	clear > /dev/tty0
-	setterm -cursor off > /dev/tty0
-	omxplayer -rb --loop --no-osd -o both /var/videos/dgzrs.mp4 > /dev/null 2> /var/log/video.log
-	clear > /dev/tty0
+	for file in $PATH/$PREFIX*; do
+		$PLAYER $OPTIONS "$PATH/$file" > /dev/null 2> $LOG
+		clear > /dev/tty0
+		$PLAYER $OPTIONS "$PAUSENVIDEO" > /dev/null 2> $LOG
+		clear > /dev/tty0
+	done
 done
